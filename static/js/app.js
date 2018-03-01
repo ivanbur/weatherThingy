4d5e8163f9d3d3d4e59fac98f6b7a0489609a4e9
ivanbur <ivan@burgert.com>
2018-02-28 20:57:33 -0800

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,7 @@ function callData() {
 		url: "http://api.openweathermap.org/data/2.5/weather?q=" + $("#user-input").val() +"&appid=9a9c0c43d75325cf5f25b4cff4bb47cb",
 		dataType: "json",
 		success: function(data) {
-			$("#countryWeather").html(data.weather[0].main + "y" + "<br> and the temperature in Celsius is: " + (data.main.temp - 273).toFixed(2));
+			$("#countryWeather").html(data.weather[0].main + "<br> and the temperature in Celsius is: " + (data.main.temp - 273).toFixed(2));
 			//$("#countryWeather").append(data.main);
 			$("#name").html(data.name);
 			console.log(data);
